efc6c5ca7d45dd570e6be7c89b24e07093ecf391
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-11-19 12:28:22 +0530

    commit 3

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="blank"></div>
+    <>
+  <Navbar title="zuckercats" />
+  </>
   );
 }
 

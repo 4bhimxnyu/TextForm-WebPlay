1be9954692b67041e818696a287ab8a4f62f5eba
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-11-22 23:42:10 +0530

    added lowercase functionality

--- myreact/src/components/Textform.js
+++ myreact/src/components/Textform.js
@@ -13,14 +13,30 @@ export default function Textform(props) {
         console.log("onchange event has been called")
         setText(event.target.value)
     }
+
+    const OnclickLower = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
             <div className="mb-3">
                 <label for="myBox" className="form-label">enter your text here for analysis</label>
                 <textarea className="form-control" id="myBox" rows="3" value={text} onChange={OnChangeFun}>This text will be dynamically changed</textarea>
             </div>
-        <button className="btn btn-primary" onClick={OnClickFun}>Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={OnClickFun}>Uppercase</button>
+        <button className="btn btn-danger" onClick={OnclickLower}>Lowercase</button>
     </div>
+    <div className='contain'>
+        <h2>Get the preview of the textForm WebPage</h2>
+        <p>Word Count : {text.split(" ").length}</p>
+        <p>Number of Characters : {text.length}</p>
+        <p>Time taken to Read all the text is {0.008 * text.split(" ").length} seconds</p>
+        
+    </div>
+    </>
   )
 }
+

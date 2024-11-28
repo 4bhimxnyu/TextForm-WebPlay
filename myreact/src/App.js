295d40e48ff81e8314f11258b10688a2a2bb1069
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-11-29 01:18:00 +0530

    first commit

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 import './App.css';
-// import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 

83ed66e427f31bd8be0c9c7585d0afb781cc73c0
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-02 11:58:55 +0530

    adding Font button

--- App/src/components/Textform.js
+++ App/src/components/Textform.js
@@ -24,12 +24,12 @@ export default function Textform(props) {
         setText(newText); 
     } 
 
-    const OnclickTrim = ()=>{
-        if (text.length>0){
-            let newText = text.trim();
-        }
-        setText(newText);
-    }
+    // const OnclickTrim = ()=>{
+    //     if (text.length>0){
+    //         let newText = text.trim();
+    //     }
+    //     setText(newText);
+    // }
 
     
   return (
@@ -43,8 +43,21 @@ export default function Textform(props) {
         <button className="btn btn-primary mx-1" onClick={OnClickFun}>Uppercase</button>
         <button className="btn btn-danger mx-1" onClick={OnclickLower}>Lowercase</button>
         <button className="btn btn-secondary mx-1" onClick={OnclickClear}>Clear</button>
-        <button className="btn btn-success mx-1" onClick={OnclickTrim}>Trim</button>
+        {/* <button className="btn btn-success mx-1" onClick={OnclickTrim}>Trim</button> */}
         {/* <button className="btn btn-alert mx-1 " onClick={InvereCase}>Inverse Case</button> */}
+        {/* <!-- Example single danger button --> */}
+        <div class="btn-group">
+        <button type="button" className="btn btn-info dropdown-toggle text-white" style={{color:props.mode==='dark'?'white':'black'}} data-bs-toggle="dropdown" aria-expanded="false">
+            Font
+        </button>
+        <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Separated link</a></li>
+        </ul>
+        </div>
     </div>
     <div className='contain' style={{color:props.mode==='dark'?'white':'black'}}>
         <h2>Get the preview of the textForm WebPage</h2>

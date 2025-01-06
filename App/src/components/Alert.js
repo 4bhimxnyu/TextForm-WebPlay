73b70c7f65fc240993399e0f12a304718a4f174f
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-01-06 13:31:15 +0530

    adding alert functionality

--- App/src/components/Alert.js
+++ App/src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Alert(props){
   return (
     <div className="alert alert-warning alert-dismissible fade show" role="alert">
-        {props.alert}
+        <Strong>{props.alert}</Strong>
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   )
